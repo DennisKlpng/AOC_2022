a7cfc24fd02e8bf1271dd5b82cc8b3922060ae1d
DennisKlpng <dennis.kloepping@web.de>
2022-12-04 12:49:24 +0100

    Day 4, using intersections again. Would be significantly easier with simple range comparisons

---
+++ AOC_4.py
@@ -0,0 +1,13 @@
+import sys
+
+
+if __name__ == '__main__':
+    indices1 = []
+    index1 = 0
+    with open(sys.argv[1], "r") as f:
+        pairs = [[list(range(int(er.split("-")[0]), int(er.split("-")[1])+1)) for er in line.split(",")] for line in
+                 f.read().splitlines()]
+        red = ["f" for pair in pairs if sorted(list(set(pair[0]).intersection(pair[1]))) in [pair[0], pair[1]]]
+        print("solution pt 1: " + str(len(red)))
+        over = ["f" for pair in pairs if len(list(set(pair[0]).intersection(pair[1]))) > 0]
+        print("solution p1 2: " + str(len(over)))

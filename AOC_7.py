c810a3c612942680d57c0b0b4b183ea1f088d5eb
DennisKlpng <dennis.kloepping@web.de>
2022-12-07 22:00:54 +0100

    Streamlined some code, added comments
    It would actually be possible to save on disk space and parsing time by removing files as nodes and just assign their size to their parent node, but this would make the tree structure less general

--- AOC_7.py
+++ AOC_7.py
@@ -1,8 +1,5 @@
 import utils
 import sys
-from collections import deque
-import re
-import copy
 
 
 def get_nodes_with_max_size(root, nodes, tgt, tgt_le=True):
@@ -15,31 +12,26 @@ def get_nodes_with_max_size(root, nodes, tgt, tgt_le=True):
 
 
 if __name__ == '__main__':
-    data = utils.read_file_as_chunk(sys.argv[1]).splitlines()
-    tree_root = utils.GenericTree('root')
-    curr_node = None
-    for line in data:
+    tree_root, curr_node = utils.GenericTree('root'), None  # both files and dirs are nodes in the tree structure
+    for line in utils.read_file_as_chunk(sys.argv[1]).splitlines():
         if line[:4] == '$ cd':
-            if line[5:].strip() == '/':
+            if line[5:] == '/':
                 curr_node = tree_root
-            elif line[5:].strip() == '..':
+            elif line[5:] == '..':
                 curr_node = curr_node.get_parent()
             else:
-                curr_node = curr_node.get_child(line[5:].strip())
+                curr_node = curr_node.get_child(line[5:])
         elif line[:4] == 'dir ':
             curr_node.add_child(line[4:])
         elif line[0].isdigit():
             curr_node.add_child(line.split(' ')[1], int(line.split(' ')[0]), True)
 
-    tree_root.fill_node_sizes()
+    tree_root.fill_node_sizes()  # parses through the tree and recursively calculates each nodes size (for dirs)
 
     node_list = []
     get_nodes_with_max_size(tree_root, node_list, 100000)
     print("Pt1: " + str(sum(node_list)))
 
-    space_needed_to_delete = tree_root.size - 40000000
     node_list_pt2 = []
-    get_nodes_with_max_size(tree_root, node_list_pt2, space_needed_to_delete, False)
+    get_nodes_with_max_size(tree_root, node_list_pt2, tree_root.size - 40000000, False)
     print("Pt2: " + str(sorted(node_list_pt2)[0]))
-
-

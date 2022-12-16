4f852ca2e862de8433926c9a867241f0815bdee3
DennisKlpng <dennis.kloepping@web.de>
2022-12-16 21:11:19 +0100

    Pt 1 solution working, slow as hell though (~11secs)

--- AOC_16.py
+++ AOC_16.py
@@ -3,6 +3,8 @@ import sys
 import re
 import numpy
 import math
+from copy import deepcopy
+import time
 
 
 def floyd_warshal(matrix):
@@ -19,20 +21,20 @@ def floyd_warshal(matrix):
 
 # returns: matrix of all travel costs (symmetrical), list of valve values with same indexing as the matrix
 def reduce_dimensionsality(input_data):
-    dim = len(data)
-    valves = [x[0] for x in data]
+    dim = len(input_data)
+    valves = [x[0] for x in input_data]
     shortest_paths = numpy.full((dim, dim), math.inf)
     rel_valves = []
     zero_valves = []
     for j in range(dim):
         shortest_paths[j, j] = 0
-        for i in range(len(data[j]) - 2):
-            elem1, elem2 = valves.index(data[j][0]), valves.index(data[j][2 + i])
+        for i in range(len(input_data[j]) - 2):
+            elem1, elem2 = valves.index(input_data[j][0]), valves.index(input_data[j][2 + i])
             shortest_paths[elem1, elem2] = 1
             shortest_paths[elem2, elem1] = 1
         # cache valve values
-        if data[j][1] != "0":
-            rel_valves.append(int(data[j][1]))
+        if input_data[j][1] != "0" or input_data[j][0] == "AA":
+            rel_valves.append(int(input_data[j][1]))
         else:
             zero_valves.append(j)
 
@@ -47,7 +49,37 @@ def reduce_dimensionsality(input_data):
     return shortest_paths, rel_valves
 
 
+def solver(pathlenghts, vals, start_index, rem_time, pt2=False):
+    stack = [(rem_time, start_index, [start_index], [], 0,)]  # ., ., visited, rates, pressure released
+    max_press = 0
+    while stack:
+        s = stack.pop()
+        if s[0] <= 0:  # no time left
+            continue
+        pot_p = sum(s[3]) * s[0]  # pot release w/o moves: rate_sum * remaining time
+        if (s[4] + pot_p) > max_press:
+            max_press = s[4] + pot_p
+        theoretical_p = s[0] * sum([r for r in vals if r not in s[2]])
+        if (theoretical_p + pot_p + s[4]) < max_press:  # throw away all solutions that can't possibly beat the max
+            continue
+        for ind in range(len(vals)):
+            if ind in s[2] or ind == start_index:  # never visit previously visited again
+                continue
+            dt = 1 + pathlengths[s[1], ind]  # time to reach and open that valve
+            p = s[4] + sum(s[3])*dt  # new pressure released: old p + sum of all rates * time_diff since old p
+            v = deepcopy(s[2])
+            r = deepcopy(s[3])
+            v.append(ind)
+            r.append(vals[ind])
+            stack.append((int(s[0] - dt), ind, v, r, p,))
+    return int(max_press)
+
+
 if __name__ == '__main__':
+    start_time = time.time()
     re_vals = re.compile('[-\\d]+|[A-Z]{2,}')
     data = [re.findall(re_vals, line) for line in utils.read_file_as_lines(sys.argv[1])]
     pathlengths, valve_vals = reduce_dimensionsality(data)
+    print(f"pt1: {solver(pathlengths, valve_vals, valve_vals.index(0), 30)}")
+    stop_time = time.time()
+    print(f"pt1 time: {stop_time - start_time}")

1bd5b8d3fc20d915746d9fb6bf9cdc0ed4e90adb
DennisKlpng <dennis.kloepping@web.de>
2022-12-21 13:12:36 +0100

    Pt 2 solution, using complex numbers to get the relation between humn and the diff

--- AOC_21.py
+++ AOC_21.py
@@ -1,19 +1,23 @@
 import utils
 import sys
 import time
-import math
-import re
 import operator
 
 
-operators = {"+": operator.add, "-": operator.sub, "/": operator.truediv, "*": operator.mul}
+operators = {"+": operator.add, "-": operator.sub, "/": operator.truediv, "*": operator.mul, "=": operator.eq}
 
 
-def calc(ops, name):
-    if ops[name].isdigit():
-        return int(ops[name])
+def calc(ops, name, pt2=False, val=0):
+    if ops[name].isdigit() or ops[name] == "1j":
+        return complex(ops[name]), 0
     num1, op, num2 = ops[name].split()
-    return int(operators[op.strip()](calc(ops, num1), calc(ops, num2)))
+    if pt2:
+        ops["humn"] = "1j"
+        num1, op, num2 = ops[name].split()
+    v1, d1 = calc(ops, num1)
+    v2, d2 = calc(ops, num2)
+    new_val = complex(operators[op.strip()](v1, v2))
+    return new_val, v1-v2
 
 
 if __name__ == '__main__':
@@ -25,11 +29,19 @@ if __name__ == '__main__':
     stop_time = time.time()
     print(f"Initialization time: {stop_time - start_time} seconds")
     start_time = stop_time
-    score = calc(op_data, "root")
+    score, diff = calc(op_data, "root")
     stop_time = time.time()
     if sys.argv[1].startswith("Test"):
         assert score == 152
-    print(f"pt1 solution: {score} time overall: {stop_time - start_time}")
+    print(f"Test: {operator.eq(4, 1)}")
+    print(f"pt1 solution: {score.real} time overall: {stop_time - start_time}")
+    start_val = 0
+    # diff with using complex numbers as the human input immediately gives the relation between the actual diff and
+    # the human influence on it
+    score, diff = calc(op_data, "root", True, start_val)
+    start_new = int(abs(diff.real / diff.imag))
 
+    if sys.argv[1].startswith("Test"):
+        assert start_new == 301
     stop_time = time.time()
-    print(f"pt2 solution: {score} time overall: {stop_time - start_time}")
+    print(f"pt2 solution: {start_new} time overall: {stop_time - start_time}")

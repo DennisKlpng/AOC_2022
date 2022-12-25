4b62ccfa3cd76c439e02dac4f31feb2e09860fb9
DennisKlpng <dennis.kloepping@web.de>
2022-12-25 01:53:45 +0100

    Replaced dict with simple set for elf pos storage, adapted vis

--- AOC_23.py
+++ AOC_23.py
@@ -1,20 +1,16 @@
 import utils
 import sys
 import time
-from collections import defaultdict
 import numpy as np
 import itertools
 
 
 def vis(dct):
-    l_vals, c_vals = [x[0] for x in dct.keys()], [x[1] for x in dct.keys()]
+    l_vals, c_vals = [x[0] for x in dct], [x[1] for x in dct]
     l_min, l_max, c_min, c_max = int(min(l_vals)), int(max(l_vals)), int(min(c_vals)), int(max(c_vals))
-    np_array_dct = np.zeros((l_max - l_min + 1, c_max - c_min + 1,))
-    for key, val in dct.items():
-        np_array_dct[(key[0] - l_min, key[1] - c_min,)] = val
     for l in range(l_max - l_min + 1):
         for c in range(c_max - c_min + 1):
-            if np_array_dct[(l, c,)] == 1:
+            if (l, c,) in dct:
                 print('#', end='')
             else:
                 print('.', end='')
@@ -26,13 +22,12 @@ if __name__ == '__main__':
     start_time = time.time()
     elves_map = np.array([[1 if x is "#" else 0 for x in line] for line in utils.read_file_as_lines(sys.argv[1])])
     diml, dimc = np.shape(elves_map)
-    elves_map_new = defaultdict(int)
+    elves_map_new = set()
     for l, c in itertools.product(range(diml), range(dimc)):
         if elves_map[(l, c)] == 1:
-            elves_map_new[(l, c, )] = elves_map[(l, c)]
+            elves_map_new.add((l, c, ))
     elves_map = elves_map_new
 
-    # vis(elves_map)
     stop_time = time.time()
     print(f"Initialization time: {stop_time - start_time} seconds")
     start_time = stop_time
@@ -47,25 +42,23 @@ if __name__ == '__main__':
         do_move = False
         # propose steps
         proposed_steps = {}
-        elves = set([key for key, val in elves_map.items() if val == 1])
-        keys = set(elves_map.keys())
-        for e in elves:
+        for e in elves_map:
             e = tuple(e)
             nb = utils.get_diagneighbours(e)
-            if keys.isdisjoint(set(nb)):
+            if elves_map.isdisjoint(set(nb)):
                 continue
             for d in range(4):
                 d = (d + counter) % 4
                 means = [sum([x[0] for x in dir_check[d]])/3, sum([x[1] for x in dir_check[d]])/3]
-                if keys.isdisjoint(set([(x[0] + e[0], x[1] + e[1],) for x in dir_check[d]])):
+                if elves_map.isdisjoint(set([(x[0] + e[0], x[1] + e[1],) for x in dir_check[d]])):
                     proposed_steps.setdefault((int(e[0] + means[0]), int(e[1] + means[1])), []).append(e)
                     break
         # check if proposed steps are unique
         for newpos, oldpos in proposed_steps.items():
             if len(oldpos) == 1:
                 do_move = True
-                del elves_map[oldpos[0]]
-                elves_map[newpos] = 1
+                elves_map.remove(oldpos[0])
+                elves_map.add(newpos)
 
         return steps + 1, do_move
 
@@ -74,7 +67,7 @@ if __name__ == '__main__':
 
     def calc_score():
         # done, calculate final result
-        l_vals, c_vals = [x[0] for x, y in elves_map.items() if y == 1], [x[1] for x, y in elves_map.items() if y == 1]
+        l_vals, c_vals = [x[0] for x in elves_map], [x[1] for x in elves_map]
         l_min, l_max, c_min, c_max = int(min(l_vals)), int(max(l_vals)), int(min(c_vals)), int(max(c_vals))
         return (l_max - l_min + 1) * (c_max - c_min + 1) - len(l_vals)
 
